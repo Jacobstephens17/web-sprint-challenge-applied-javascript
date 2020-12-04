d80a80eecffb56d62a192782b6945825d9f60ed4
Jacob Stephens <jacobstephens2@gmail.com>
2020-12-04 08:36:50 -0800

    finished heading creator, needs styling but going to save that for the end

--- components/Header.js
+++ components/Header.js
@@ -11,19 +11,29 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
+const headContainer = document.querySelector('.header-container');
+
 function Header() {
     const header = document.createElement('div');
     const date = document.createElement('span');
     const h1 = document.createElement('h1');
     const temp = document.createElement('span');
 
+    date.textContent = 'March 28, 2020';
+    h1.textContent = 'Lambda Times';
+    temp.textContent = '98';
+
     header.className.add = 'header';
     date.className.add = 'date';
     temp.className.add = 'temp';
 
-
+    header.appendChild(h1);
+    header.appendChild(temp);
     header.appendChild(date);
-    date.appendChild(h1);
-    date.appendChild(temp);
-    
+
+    headContainer.appendChild(header);
 }
+
+const newH = Header();
+
+headContainer.appendChild(newH);
